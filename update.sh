d0218e513ceab8ade8bc53e760bc347ba2589dee
kuilin <kuilin@gmail.com>
2024-07-26 15:11:16 +0000

    Revert "Not sure what's up with their cache"
    This reverts commit 84a91ef95ba70303b7c6d08401153b84b9c0309d.
    
    Whoops, the deletions weren't being staged. And it seems like they fixed the cache issue, so yay - the git diff looks better, so let's put it back.
    
    This commit moves the previous one current-e3c58dc36b08e436e33fbe482a7d2ddaf0156623.json to current.json, so the latest update (~10 hours ago) will be replayed.

--- update.sh
+++ update.sh
@@ -10,10 +10,7 @@ curl -s 'https://api.capitalone.com/deposits/products/~/search' \
   -H 'Content-Type: application/json' \
   --data-raw '{"include":["RATES"],"isRenewableRate":true}' | jq 'del(.lastCachedAt)' > current.json
 
-mv current.json "current-$(cat current.json | sha1sum | cut -d' ' -f1).json"
-find current-*.json -type f -mtime +1 -delete
-
-git add current-*.json &&
+git add current.json &&
   GIT_AUTHOR_NAME="auto" \
   GIT_AUTHOR_EMAIL="" \
   GIT_COMMITTER_NAME="auto" \
